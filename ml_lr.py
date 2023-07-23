e1c7654ad17dc2bf5b26ad8fab2a8592af1392f4
ketan8157 <60535051+ketan8157@users.noreply.github.com>
2023-07-23 19:20:46 +0530

    ml_lr.py

--- ml_lr.py
+++ ml_lr.py
@@ -1,6 +1,6 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-data=pd.read_csv("SLR_ML_data - Sheet1.csv")
+data=pd.read_csv("ml_lr_data.csv")
 x=data[["hours"]]
 y=data["marks"]
 model=LinearRegression()
